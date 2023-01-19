a800af871725a81a826dc0e0f704d43cf2113db4
Aman Panda <aman.panda@learner.manipal.edu>
2023-01-19 22:49:19 +0530

    First commit

---
+++ demo.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello People");
+        System.out.println("My name is Aman.");
+    }
+}
